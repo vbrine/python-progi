71e240b8ba8b79eafb97c530f4cf8ec0169eb03f
Bence <csepelibence0@gmail.com>
2023-04-25 12:04:52 +0200

    Kiegészítés

--- csgo.py
+++ csgo.py
@@ -1,15 +1,17 @@
 class CsGo:
-    def __init__(self,name,damage,price,acc,fr,type,poo):
+    def __init__(self,name,damage,price,acc,fr,type):
       self.name=oszlop[0]
       self.damage=int(oszlop[1])
       self.price=int(oszlop[2])
       self.acc=float(oszlop[3])
       self.fr=float(oszlop[4])
       self.type=oszlop[5]
-      self.poo=oszlop[6]
+      # self.poo=oszlop[6]
 csgo=[]
 fajl=open("csgo.txt","r",encoding="utf-8")
-readline()
+fajl.readline()
 for sor in fajl:
     oszlop=sor.strip().split(";")
-    csgo.append(CsGo(oszlop[0],oszlop[1],oszlop[2],oszlop[3],oszlop[4],oszlop[5],oszlop[6]))
+    csgo.append(CsGo(oszlop[0],oszlop[1],oszlop[2],oszlop[3],oszlop[4],oszlop[5]))
+for i in csgo:
+      print(f"{i.name} {i.damage} {i.price}$ {i.acc} {i.fr} {i.type}")
